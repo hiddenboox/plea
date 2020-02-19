4cd43a754ad24998f83f47ab2890fd02e45cb5a9
Sebastian Sobociński <sebastian@soft-bind.com>
2020-02-19 14:24:04 +0100

    build(rollup): use named exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,10 @@ import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
 export default {
-  input: 'src/plea.js',
+  input: 'src/index.js',
   external: ['querystring', 'https', 'url', 'assert'],
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'es' },
   ],
   plugins: [babel()],
